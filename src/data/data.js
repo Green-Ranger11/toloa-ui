8c9468b301b2454125c17269dfc8edf6fc214bef
Green-Ranger11 <junior.eteuati@gmail.com>
2022-04-07 02:19:11 +1300

    Add country flags

---
+++ src/data/data.js
@@ -0,0 +1,535 @@
+export const nodes = [
+    {
+        id: 2,
+        name: 'SPC',
+        val: 20,
+        img: 'spc.png',
+    },
+    {
+        id: 3,
+        name: 'PIF',
+        val: 20,
+        img: 'pif.png',
+    },
+    {
+        id: 4,
+        name: 'UNDP',
+        val: 20,
+        img: 'un.png',
+    },
+    {
+        id: 5,
+        name: 'USP',
+        val: 20,
+        img: 'usp.png',
+    },
+    {
+        id: 6,
+        name: 'EU',
+        val: 20,
+        img: 'eu.png',
+    },
+    {
+        id: 7,
+        name: 'Samoa',
+        val: 5,
+        img: 'ws.svg',
+    },
+    {
+        id: 8,
+        name: 'Fiji',
+        val: 5,
+        img: 'fj.svg',
+    },
+    {
+        id: 9,
+        name: 'Tonga',
+        val: 5,
+        img: 'to.svg',
+    },
+    {
+        id: 10,
+        name: 'Vanuatu',
+        val: 5,
+        img: 'vu.svg',
+    },
+    {
+        id: 11,
+        name: 'Rising sea levels in the Pacific',
+        value: 1,
+        color: "#a6cee3"
+    },
+    {
+        id: 12,
+        name: 'Early childcare in the Pacific',
+        value: 1,
+        color: "#a6cee3"
+    },
+    {
+        id: 13,
+        name: 'Economic Planning in the Pacific',
+        value: 1,
+        color: "#a6cee3"
+    },
+    {
+        id: 14,
+        name: 'Regional Development',
+        value: 1,
+        color: "#a6cee3"
+    },
+    {
+        id: 15,
+        name: 'Lorem ipsum',
+        value: 1,
+        color: "#a6cee3",
+    },
+    {
+        id: 16,
+        name: 'Lorem ipsum',
+        value: 1,
+        color: "#a6cee3"
+    },
+    {
+        id: 17,
+        name: 'Lorem ipsum',
+        value: 1,
+        color: "#a6cee3"
+    },
+    {
+        id: 18,
+        name: 'Lorem ipsum',
+        value: 1,
+        color: "#a6cee3"
+    },
+    {
+        id: 19,
+        name: 'Lorem ipsum',
+        value: 1,
+        color: "#a6cee3"
+    },
+    {
+        id: 20,
+        name: 'Lorem ipsum',
+        value: 1,
+        color: "#a6cee3"
+    },
+    {
+        id: 21,
+        name: 'Lorem ipsum',
+        value: 1,
+        color: "#a6cee3"
+    },
+    {
+        id: 22,
+        name: 'Lorem ipsum',
+        value: 1,
+        color: "#a6cee3"
+    },
+    {
+        id: 23,
+        name: 'Lorem ipsum',
+        value: 1,
+        color: "#a6cee3"
+    },
+    {
+        id: 24,
+        name: 'Lorem ipsum',
+        value: 1,
+        color: "#a6cee3"
+    },
+    {
+        id: 25,
+        name: 'Lorem ipsum',
+        value: 1,
+        color: "#a6cee3"
+    },
+    {
+        id: 26,
+        name: 'Lorem ipsum',
+        value: 1,
+        color: "#4863fa"
+    },
+    {
+        id: 27,
+        name: 'Lorem ipsum',
+        value: 1,
+        color: "#4863fa"
+    },
+    {
+        id: 28,
+        name: 'Lorem ipsum',
+        value: 1,
+        color: "#4863fa"
+    },
+    {
+        id: 29,
+        name: 'Lorem ipsum',
+        value: 1,
+        color: "#4863fa"
+    },
+    {
+        id: 30,
+        name: 'Lorem ipsum',
+        value: 1,
+        color: "#4863fa"
+    },
+    {
+        id: 31,
+        name: 'Lorem ipsum',
+        value: 1,
+        color: "#4863fa"
+    },
+    {
+        id: 32,
+        name: 'Lorem ipsum',
+        value: 1,
+        color: "#4863fa"
+    },
+    {
+        id: 33,
+        name: 'Lorem ipsum',
+        value: 1,
+        color: "#4863fa"
+    },
+    {
+        id: 34,
+        name: 'Lorem ipsum',
+        value: 1,
+        color: "#4863fa"
+    },
+    {
+        id: 35,
+        name: 'Lorem ipsum',
+        value: 1,
+        color: "#4863fa"
+    },
+
+    {
+        id: 36,
+        name: 'Lorem ipsum',
+        value: 1,
+        color: "#4863fa"
+    },
+    {
+        id: 37,
+        name: 'Lorem ipsum',
+        value: 1,
+        color: "#4863fa"
+    },
+];
+
+export const links = [
+    {
+        source: 2,
+        target: 11,
+    },
+    {
+        source: 2,
+        target: 12,
+    },
+    {
+        source: 2,
+        target: 21,
+    },
+    {
+        source: 2,
+        target: 23,
+    },
+    {
+        source: 2,
+        target: 24,
+    },
+    {
+        source: 2,
+        target: 20,
+    },
+    {
+        source: 2,
+        target: 24,
+    },
+    {
+        source: 2,
+        target: 27,
+    },
+    {
+        source: 2,
+        target: 32,
+    },
+    {
+        source: 2,
+        target: 33,
+    },
+    {
+        source: 2,
+        target: 34,
+    },
+    {
+        source: 2,
+        target: 37,
+    },
+    {
+        source: 3,
+        target: 13,
+    },
+    {
+        source: 3,
+        target: 20,
+    },
+    {
+        source: 3,
+        target: 22,
+    },
+    {
+        source: 3,
+        target: 26,
+    },
+    {
+        source: 3,
+        target: 27,
+    },
+    {
+        source: 3,
+        target: 29,
+    },
+    {
+        source: 3,
+        target: 31,
+    },
+    {
+        source: 3,
+        target: 37,
+    },
+    {
+        source: 3,
+        target: 34,
+    },
+    {
+        source: 3,
+        target: 35,
+    },
+    {
+        source: 3,
+        target: 25,
+    },
+    {
+        source: 4,
+        target: 12,
+    },
+    {
+        source: 4,
+        target: 14,
+    },
+    {
+        source: 4,
+        target: 15,
+    },
+    {
+        source: 4,
+        target: 19,
+    },
+    {
+        source: 4,
+        target: 21,
+    },
+    {
+        source: 4,
+        target: 22,
+    },
+    {
+        source: 4,
+        target: 23,
+    },    
+    {
+        source: 4,
+        target: 26,
+    },       
+    {
+        source: 4,
+        target: 28,
+    },       
+    {
+        source: 4,
+        target: 29,
+    },       
+    {
+        source: 4,
+        target: 32,
+    },       
+    {
+        source: 4,
+        target: 33,
+    },       
+    {
+        source: 4,
+        target: 34,
+    },           
+    {
+        source: 5,
+        target: 11,
+    },    
+    {
+        source: 4,
+        target: 13,
+    },    
+    {
+        source: 4,
+        target: 14,
+    },
+    {
+        source: 4,
+        target: 18,
+    },
+    {
+        source: 4,
+        target: 21,
+    },
+    {
+        source: 5,
+        target: 17,
+    },
+    {
+        source: 5,
+        target: 18,
+    },
+    {
+        source: 5,
+        target: 19,
+    },
+    {
+        source: 5,
+        target: 20,
+    },
+    {
+        source: 5,
+        target: 21,
+    },
+    {
+        source: 5,
+        target: 22,
+    },
+    {
+        source: 5,
+        target: 23,
+    },
+    {
+        source: 5,
+        target: 24,
+    },
+    {
+        source: 5,
+        target: 28,
+    },
+    {
+        source: 5,
+        target: 29,
+    },
+    {
+        source: 5,
+        target: 30,
+    },
+    {
+        source: 5,
+        target: 31,
+    },
+    {
+        source: 6,
+        target: 11,
+    },
+    {
+        source: 6,
+        target: 12,
+    },
+    {
+        source: 6,
+        target: 13,
+    },
+    {
+        source: 6,
+        target: 14,
+    },
+    {
+        source: 6,
+        target: 16,
+    },
+    {
+        source: 6,
+        target: 34,
+    },
+    {
+        source: 6,
+        target: 35,
+    },
+    {
+        source: 6,
+        target: 36,
+    },
+    {
+        source: 6,
+        target: 37,
+    },
+    {
+        source: 6,
+        target: 33,
+    },
+    {
+        source: 7,
+        target: 26
+    },
+    {
+        source: 7,
+        target: 28
+    },
+    {
+        source: 7,
+        target: 33
+    },
+    {
+        source: 7,
+        target: 34
+    },
+    {
+        source: 7,
+        target: 37
+    },
+    {
+        source: 8,
+        target: 27,
+    },
+    {
+        source: 8,
+        target: 29,
+    },
+    {
+        source: 8,
+        target: 30,
+    },
+    {
+        source: 8,
+        target: 31,
+    },
+    {
+        source: 9,
+        target: 35,
+    },
+    {
+        source: 9,
+        target: 37,
+    },
+    {
+        source: 10,
+        target: 27,
+    },
+    {
+        source: 10,
+        target: 30,
+    },
+    {
+        source: 10,
+        target: 32,
+    },
+    {
+        source: 10,
+        target: 29,
+    },
+    
+]
