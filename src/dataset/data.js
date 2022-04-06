703c2f75359baf9537d172f67effb4572ab85d27
Green-Ranger11 <junior.eteuati@gmail.com>
2022-04-06 14:33:29 +1300

    Add `react-force-graph`

---
+++ src/dataset/data.js
@@ -0,0 +1,103 @@
+export const nodes = [
+    {
+        id: 2,
+        name: 'SPC',
+        val: 20,
+        img: 'spc.png',
+    },
+    {
+        id: 3,
+        name: 'PIF',
+        val: 20,
+        img: 'pif.png',
+    },
+    {
+        id: 4,
+        name: 'UNDP',
+        val: 20,
+        img: 'un.png',
+    },
+    {
+        id: 5,
+        name: 'USP',
+        val: 20,
+        img: 'usp.png',
+    },
+    {
+        id: 6,
+        name: 'EU',
+        val: 20,
+        img: 'eu.png',
+    },
+    {
+        id: 7,
+        name: 'Samoa',
+        val: 5,
+    },
+    {
+        id: 8,
+        name: 'Fiji',
+        val: 5,
+    },
+    {
+        id: 9,
+        name: 'Tonga',
+        val: 5,
+    },
+    {
+        id: 10,
+        name: 'Vanuatu',
+        val: 5,
+    }
+];
+
+export const links = [
+    {
+        source: 2,
+        target: 7,
+    },
+    {
+        source: 2,
+        target: 8,
+    },
+    {
+        source: 2,
+        target: 9,
+    },
+    {
+        source: 3,
+        target: 8,
+    },
+    {
+        source: 3,
+        target: 9,
+    },
+    {
+        source: 3,
+        target: 10,
+    },
+    {
+        source: 4,
+        target: 7,
+    },
+    {
+        source: 4,
+        target: 8,
+    },
+    {
+        source: 5,
+        target: 9,
+    },
+    {
+        source: 5,
+        target: 10,
+    },
+    {
+        source: 6,
+        target: 7,
+    },
+    {
+        source: 6,
+        target: 8,
+    },
+]
--- src/pages/Overview.jsx
+++ src/pages/Overview.jsx
@@ -1,11 +1,26 @@
-import React from 'react';
+import React from "react";
+import useEffect from "react";
+import { nodes, links } from "../dataset/data";
+import { ForceGraph3D } from "react-force-graph";
+import { TextureLoader, SpriteMaterial, Sprite } from "three";
+import spc from "../images/spc.png";
 
 function Overview() {
   return (
     <>
-      Topics
+      <ForceGraph3D
+        graphData={{ nodes, links }}
+        nodeThreeObject={(node) => {
+          const imgTexture = new TextureLoader().load(node.img ? new URL(`../images/${node.img}`, import.meta.url).href : spc);
+          const material = new SpriteMaterial({ map: imgTexture });
+          const sprite = new Sprite(material);
+          sprite.scale.set(12, 12);
+
+          return sprite;
+        }}
+      />
     </>
   );
-};
+}
 
 export default Overview;
