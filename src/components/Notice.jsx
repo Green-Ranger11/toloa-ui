b53c2b32618f0a2fb0ee32c5970ca4faef7789df
Chia Lin <chia.lin685@gmail.com>
2022-04-08 07:33:17 +1200

    Add Notice component

--- src/components/Notice.jsx
+++ src/components/Notice.jsx
@@ -1,11 +1,42 @@
 import React, { useState } from "react";
-import Card from "../partials/Card";
+import { SpeakerphoneIcon } from "@heroicons/react/solid";
+import { XIcon } from "@heroicons/react/outline";
 
 function Notice() {
+  const [showNotice, setShowNotice] = useState(true);
+
+  const handleNoticeClick = () => {
+    setShowNotice(!showNotice);
+  };
+
   return (
-    <Card>
-      <div className="bg-indigo-600">this is notice</div>
-    </Card>
+    <>
+      {showNotice && (
+        <div className="bg-indigo-600 m-3 py-2 px-1 rounded-md">
+          <div className="max-w-7xl mx-auto py-3 px-0 sm:px-3 lg:px-2">
+            <div className="flex items-center justify-between flex-wrap">
+              <div className="w-0 flex-1 flex items-center">
+                <span className="flex items-center">
+                  <SpeakerphoneIcon className="h-5 w-5 text-white " />
+                  <p className="ml-3 font-medium text-white truncate">
+                    <span className="text-sm text-gray-100">
+                      Click here to see an overview of discussions and
+                      contributions
+                    </span>
+                  </p>
+                </span>
+              </div>
+              <div>
+                <XIcon
+                  className="h-6 w-6 text-white"
+                  onClick={handleNoticeClick}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
 
