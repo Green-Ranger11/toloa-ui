77a3096e29bb8fc665c79ff769abbfc98071495a
Chia Lin <chia.lin685@gmail.com>
2022-04-08 07:33:17 +1200

    Add Notice component

--- src/pages/Feed.jsx
+++ src/pages/Feed.jsx
@@ -11,7 +11,7 @@ function Feed() {
     <div className="flex flex-col min-h-screen overflow-hidden">
       <Header />
       <main className="flex-grow mt-[80px]">
-        <div className="flex flex-wrap -mb-4 mx-auto container justify-center">
+        <div className="flex flex-wrap -mb-4 mx-auto container justify-center bg-slate-300">
           <div className="w-full sm:w-1/2 md:w-1/5 mb-4 px-2 h-screen bg-transparent">
             <Card>
               <Menu />
