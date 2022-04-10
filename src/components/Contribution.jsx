5b4be4d2e01d12cb5c0e5669ce61268fc512df20
shifu-lin <chia.lin685@gmail.com>
2022-04-11 09:08:58 +1300

    Added form for contribution/discussion

--- src/components/Contribution.jsx
+++ src/components/Contribution.jsx
@@ -11,6 +11,46 @@ function Contribution() {
         <LightBulbIcon className="text-gray-500 w-6" />
       </div>
       <form>
+        <div className="mb-4 mx-3">
+          <label
+            className="block text-gray-500 text-sm font-bold mb-2"
+            for="username"
+          >
+            Title
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline placeholder:text-sm border-gray-400"
+            type="text"
+            placeholder="Enter title"
+          />
+        </div>
+        <div className="mb-4 mx-3">
+          <label
+            className="block text-gray-500 text-sm font-bold mb-2"
+            for="username"
+          >
+            Title
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline placeholder:text-sm border-gray-400"
+            type="text"
+            placeholder="Enter title"
+          />
+        </div>
+        <div className="mb-4 mx-3">
+          <label
+            className="block text-gray-500 text-sm font-bold mb-2"
+            for="username"
+          >
+            Title
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline placeholder:text-sm border-gray-400"
+            type="text"
+            placeholder="Enter title"
+          />
+        </div>
+
         <button
           type="submit"
           className="text-[#fff] w-[100%] py-2 rounded-md bg-indigo-500"
