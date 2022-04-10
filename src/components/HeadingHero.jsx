4fa6dc2e15a7c00f241ceb910f5d795845ee7dd1
shifu-lin <chia.lin685@gmail.com>
2022-04-11 09:08:58 +1300

    Added form for contribution/discussion

---
+++ src/components/HeadingHero.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { BadgeCheckIcon } from "@heroicons/react/outline";
+import Card from "./Card";
+
+function HeadingHero({ title, description }) {
+  return (
+    <Card>
+      <div className="flex flex-wrap justify-between items-center px-3 pt-3 pb-3">
+        <h3 className="text-sm font-semibold text-gray-500 uppercase">
+          {title}
+        </h3>
+        <BadgeCheckIcon className="w-5" />
+      </div>
+      <div className="flex px-3 pb-3 text-sm text-gray-500">{description}</div>
+    </Card>
+  );
+}
+
+export default HeadingHero;
--- src/pages/Timeline.jsx
+++ src/pages/Timeline.jsx
@@ -4,6 +4,7 @@ import Footer from "../components/Footer";
 import PostTimeline from "../components/PostTimeline";
 import Contribution from "../components/Contribution";
 import Card from "../components/Card";
+import HeadingHero from "../components/HeadingHero";
 
 function Timeline() {
   return (
@@ -12,6 +13,10 @@ function Timeline() {
 
       <div className="w-[80%] max-w-[100%] mx-auto grid sm:grid-cols-12 md-grid-cols-12 pt-6 gap-8 mt-[80px] ">
         <div className="rounded border-gray-300 border-2 border-dashed col-span-8">
+          <HeadingHero
+            title="Climate Change and its Impacts for SIDS"
+            description="Explore the effects of climate change and how it is directly impacting low-lying areas of the Pacific Region. "
+          />
           <PostTimeline />
         </div>
         <div className="rounded border-gray-300 border-2 border-dashed col-span-4">
