24559dc3d3e00aab5f40374d443f11f8791ad6c3
shifu-lin <chia.lin685@gmail.com>
2022-04-11 21:58:17 +1400

    Add Contribution form submit

--- src/components/PostTimeline.jsx
+++ src/components/PostTimeline.jsx
@@ -1,6 +1,6 @@
 import React, { useContext } from "react";
 import ContributionContext from "../context/ContributionContext";
-import { ArchiveIcon } from "@heroicons/react/outline";
+import { ArchiveIcon, BanIcon } from "@heroicons/react/outline";
 
 function PostTimeline() {
   const { contribution } = useContext(ContributionContext);
@@ -8,7 +8,19 @@ function PostTimeline() {
 
   return (
     <>
-      <ol className="relative border-l border-gray-200 dark:border-gray-700 w-[80%] mx-auto">
+      <ol className="relative border-l border-gray-200 dark:border-gray-700 w-[80%] mx-auto ">
+        <li className="mb-10 ml-6">
+          <span className="flex absolute -left-3 justify-center text-white items-center w-6 h-6 bg-green-600 rounded-full ring-8 ring-green-300">
+            <BanIcon />
+          </span>
+
+          <h3 className="flex items-center mb-1 text-lg font-semibold text-gray-600 ">
+            Start
+            <span className="bg-green-200 text-blue-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded ml-3">
+              Final
+            </span>
+          </h3>
+        </li>
         {contribution.map((cont) => (
           <li className="mb-10 ml-6">
             <span className="flex absolute -left-3 justify-center text-white items-center w-6 h-6 bg-blue-600 rounded-full ring-8 ring-blue-300">
@@ -23,7 +35,12 @@ function PostTimeline() {
             </h3>
 
             <time className="block mb-2 text-sm font-normal leading-none text-gray-500 ">
-              {cont.createdAt}
+              {new Date(cont.createdAt).getFullYear() +
+                "-" +
+                new Date(cont.createdAt).getMonth() +
+                "-" +
+                new Date(cont.createdAt).getDate()}
+              {/* {cont.createdAt} */}
             </time>
 
             <p className="mb-4 text-base font-normal text-gray-400">
@@ -31,6 +48,18 @@ function PostTimeline() {
             </p>
           </li>
         ))}
+        <li className="mb-10 ml-6">
+          <span className="flex absolute -left-3 justify-center text-white items-center w-6 h-6 bg-red-600 rounded-full ring-8 ring-red-300">
+            <BanIcon />
+          </span>
+
+          <h3 className="flex items-center mb-1 text-lg font-semibold text-gray-600 ">
+            Present
+            <span className="bg-red-200 text-blue-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded ml-3">
+              Final
+            </span>
+          </h3>
+        </li>
       </ol>
     </>
   );
