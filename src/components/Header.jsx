9f72b0a5760207f83f0753a1a219cecea109e835
Andre Westerlund <andrewest_04@hotmail.com>
2022-04-12 12:25:57 +1300

    Add Rendering Logic for Header Buttons

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,16 @@
 import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import Logo from '../images/logo.png';
 
 function Header() {
+  const { pathname  } = useLocation();
   const [top, setTop] = useState(true);
+  const [isLoggedIn, setIsLoggedIn] = useState(['feed', 'timeline', 'overview'].includes(pathname.slice(1)));
+
+  const checkIsLoggedIn = () => {
+    let isAuth = ['feed', 'timeline', 'overview'].includes(pathname.slice(1));
+    setIsLoggedIn(isAuth);
+  }
 
   // detect whether user has scrolled the page down by 10px
   useEffect(() => {
@@ -11,48 +18,53 @@ function Header() {
       window.pageYOffset > 10 ? setTop(false) : setTop(true);
     };
     window.addEventListener("scroll", scrollHandler);
+    checkIsLoggedIn();
     return () => window.removeEventListener("scroll", scrollHandler);
-  }, [top]);
+  }, [top, pathname]);
 
   return (
     <header className={`fixed w-full z-30 md:bg-opacity-10 transition duration-300 ease-in-out ${!top && 'bg-white backdrop-blur-sm shadow-lg'}`}>
-      <div className="max-w-6xl mx-auto px-5 sm:px-6">
+      <div className="max-w-6xl px-5 mx-auto sm:px-6">
         <div className="flex items-center justify-between h-16 md:h-20">
           {/* Site branding */}
           <div className="flex-shrink-0 mr-4">
             {/* Logo */}
             <Link to="/" className="block" aria-label="Toloa">
-              <img className="h-14 w-auto" src={Logo} alt="Toloa" />
+              <img className="w-auto h-14" src={Logo} alt="Toloa" />
             </Link>
           </div>
 
           {/* Site navigation */}
           <nav className="flex flex-grow">
-            <ul className="flex flex-grow justify-end flex-wrap items-center">
-              <li>
+            <ul className="flex flex-wrap items-center justify-end flex-grow">
+              { !isLoggedIn &&
+                <li>
                 <Link
                   to="/signin"
-                  className="font-medium text-gray-600 hover:text-gray-900 px-5 py-3 flex items-center transition duration-150 ease-in-out"
+                  className="flex items-center px-5 py-3 font-medium text-gray-600 transition duration-150 ease-in-out hover:text-gray-900"
                 >
                   Sign in
                 </Link>
               </li>
+              }
               <li>
                 <Link
                   to="/signup"
-                  className="btn-sm text-gray-200 bg-gray-900 hover:bg-gray-800 ml-3"
+                  className="ml-3 text-gray-200 bg-gray-900 btn-sm hover:bg-gray-800"
                 >
-                  <span>Sign Out</span>
-                  <svg
-                    className="w-3 h-3 fill-current text-gray-400 flex-shrink-0 ml-2 -mr-1"
+                  <span>{isLoggedIn ? 'Sign Out': 'Sign Up'}</span>
+                  {isLoggedIn &&
+                    <svg
+                    className="flex-shrink-0 w-3 h-3 ml-2 -mr-1 text-gray-400 fill-current"
                     viewBox="0 0 12 12"
                     xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      d="M11.707 5.293L7 .586 5.586 2l3 3H0v2h8.586l-3 3L7 11.414l4.707-4.707a1 1 0 000-1.414z"
-                      fillRule="nonzero"
-                    />
-                  </svg>
+                    >
+                      <path
+                        d="M11.707 5.293L7 .586 5.586 2l3 3H0v2h8.586l-3 3L7 11.414l4.707-4.707a1 1 0 000-1.414z"
+                        fillRule="nonzero"
+                      />
+                    </svg>
+                  }
                 </Link>
               </li>
             </ul>
