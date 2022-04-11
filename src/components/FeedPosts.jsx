7e0402bab5cee08a7b977ad17e7b25b6dce78487
Andre Westerlund <andrewest_04@hotmail.com>
2022-04-12 11:54:39 +1300

    Fetch Topics from Api for Feed Posts

--- src/components/FeedPosts.jsx
+++ src/components/FeedPosts.jsx
@@ -1,21 +1,46 @@
 import React from "react";
 import Post from "./Post";
+import axios from 'axios';
+import { format, parseISO } from 'date-fns';
 import eu from "../images/eu.png";
 import spc from "../images/spc.png";
 import usp from "../images/usp.png";
 import un from "../images/un.png";
 
 function FeedPosts() {
+  const [topics, setTopics] = React.useState([]);
+
+  React.useEffect(() => {
+        const fetchTopics = async () => {
+                const result = await axios.get('http://localhost:5000/topic');
+                const fetchedTopics = result.data.map(topic => {
+                        return {
+                                id: topic.id,
+                                title: topic.title,
+                                description: topic.description,
+                                author: `${topic?.createdBy?.firstName} ${topic?.createdBy?.lastName}`,
+                                date: format(parseISO(topic.createdAt), 'd MMM'),
+                        }
+                });
+                setTopics(fetchedTopics);
+        }
+
+        fetchTopics();
+  }, [])
+
   return (
     <>
-      <Post title="Health & NCDs" author="Alan Manaia" date="8 Apr" description="Explore the rise of Non-communicable diseases in the Pacific and
-              what we can do to solve this issues..." />
-      <Post title="Climate Change" author="John Doe" date="8 Apr" description="Explore the rise of Non-communicable diseases in the Pacific and
-              what we can do to solve this issues..." />
-      <Post title="Education" author="Jane Doe" date="8 Apr" description="Explore the rise of Non-communicable diseases in the Pacific and
-              what we can do to solve this issues..." />
-      <Post title="Information Tech" author="Alex Alexander" date="8 Apr" description="Explore the rise of Non-communicable diseases in the Pacific and
-              what we can do to solve this issues..." />
+    {
+            topics?.length > 0 && topics?.map(topic => (
+                <Post
+                        key={topic?.id}
+                        title={topic?.title}
+                        description={topic?.description}
+                        date={topic?.date}
+                        author = {topic?.author}
+                />
+            ))
+    }
     </>
   );
 }
