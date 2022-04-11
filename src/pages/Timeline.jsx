c1961efc235ab883d88966034a78806455288829
Green-Ranger11 <junior.eteuati@gmail.com>
2022-04-12 12:39:40 +1300

    Fix footer placement

--- src/pages/Timeline.jsx
+++ src/pages/Timeline.jsx
@@ -10,25 +10,26 @@ import Discussion from "../components/Discussion";
 
 function Timeline() {
   return (
-    <div className="w-[80%] max-w-[100%] mx-auto grid sm:grid-cols-12 md-grid-cols-12 pt-6 gap-8 mt-[80px] ">
-      <div className="rounded border-gray-300 border-2 border-dashed col-span-8">
-        <HeadingHero
-          title="Climate Change and its Impacts for SIDS"
-          description="Explore the effects of climate change and how it is directly impacting low-lying areas of the Pacific Region. "
-        />
-        <PostTimeline />
+    <>
+      <div className="w-[80%] max-w-[100%] mx-auto grid sm:grid-cols-12 md-grid-cols-12 pt-6 gap-8 mt-[80px] ">
+        <div className="rounded border-gray-300 border-2 border-dashed col-span-8">
+          <HeadingHero
+            title="Climate Change and its Impacts for SIDS"
+            description="Explore the effects of climate change and how it is directly impacting low-lying areas of the Pacific Region. "
+          />
+          <PostTimeline />
+        </div>
+        <div className="rounded border-gray-300 border-2 border-dashed col-span-4">
+          <Card>
+            <Contribution />
+          </Card>
+          <Card>
+            <Discussion />
+          </Card>
+        </div>
       </div>
-      <div className="rounded border-gray-300 border-2 border-dashed col-span-4">
-        <Card>
-          <Contribution />
-        </Card>
-        <Card>
-          <Discussion />
-        </Card>
-      </div>
-
       <Footer />
-    </div>
+    </>
   );
 }
 
